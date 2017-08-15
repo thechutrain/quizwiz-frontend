223759ac5654c6bb2c83367bd8c17eb6772fe662
thechutrain <chu.chuyalan@gmail.com>
2017-08-14 21:03:52 -0400

    added an x to the full screen menu and transition in and out now

--- src/static/js/test1.js
+++ src/static/js/test1.js
@@ -1,11 +1,11 @@
-// alert('hello there!!! Check out the console foo')
-console.log('This is from test1.js file')
-class Person {
-	constructor(name, age) {
-		this.name = name
-		this.age = age
-	}
-}
-
-const jon = new Person('jon', 77)
-console.log(typeof jon)
+// // alert('hello there!!! Check out the console foo')
+// console.log('This is from test1.js file')
+// class Person {
+// 	constructor(name, age) {
+// 		this.name = name
+// 		this.age = age
+// 	}
+// }
+//
+// const jon = new Person('jon', 77)
+// console.log(typeof jon)
--- src/static/js/test2.js
+++ src/static/js/test2.js
@@ -1,2 +1,33 @@
-console.log('hello world')
-console.log('goodbye')
+document.addEventListener('DOMContentLoaded', function() {
+	// alert('DOMContentLoaded')
+	// variables
+	var hamburgerIcon = document.querySelector('img.menu-icon')
+	var exitIcon = document.querySelector('img.x-icon')
+	var fsMenuContainer = document.querySelector('div.fullScreenMenuContainer')
+
+	// Event Listeners
+	function openFullMenu() {
+		alert('opening full menu')
+	}
+
+	// add event listeners
+	hamburgerIcon.addEventListener('click', function() {
+		var classNameArray = fsMenuContainer.className.split(' ')
+		var hiddenIndex = classNameArray.indexOf('hidden')
+		if (hiddenIndex !== -1) {
+			// remove that class name
+			classNameArray.splice(hiddenIndex, 1)
+			fsMenuContainer.setAttribute('class', classNameArray.join(' '))
+		}
+	})
+	exitIcon.addEventListener('click', function() {
+		var classNameArray = fsMenuContainer.className.split(' ')
+		classNameArray.push('hidden')
+		var classNameStr = classNameArray.join(' ')
+		fsMenuContainer.setAttribute('class', classNameStr)
+	})
+
+	// fsMenuContainer.addEventListener('click', function() {
+	// 	alert('fsMenuContainer was clicked!')
+	// })
+})
